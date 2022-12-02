f184b0b574f409ac7eb4b994b23f26eb82381668
Josué Makuta <makutajosue@gmail.com>
2022-12-02 14:21:40 +0100

    Initial commit ...

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Welcom to our product Page ...
+      </h1>
+    </div>
+  )
+}
+
+export default App
